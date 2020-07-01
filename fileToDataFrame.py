60e20b3c81d39aab82225f2b93b2faf1c5c82110
c121034t@icloud.com <c121034t@icloud.com>
2020-07-01 13:50:38 +0900

    テキストファイルをデータフレームに格納する関数を作った #1

---
+++ fileToDataFrame.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from email.parser import Parser
+
+def fileToDataFrame(file_list:list) -> pd.DataFrame:
+    cols = ['Path', 'Message-ID', 'Date', 'From', 'To', 'Cc', 'Bcc', 'X-From', 'X-To', 'X-cc', 'X-bcc', 'Subject', 'Message']
+    df = pd.DataFrame(index=[], columns=cols)
+    
+    for file in file_list:
+        with open(file) as f:
+            mail = Parser().parse(f)
+        record = {}
+        for header in cols:
+            if header == 'Path':
+                record[header] = file
+            elif header == 'Message':
+                record[header] = mail.get_payload()
+            else:
+                record[header]=mail.get(header)
+        df = df.append(record, ignore_index=True)
+    return df
+
+if __name__ == '__main__':
+    fileToDataFrame(file_list)
