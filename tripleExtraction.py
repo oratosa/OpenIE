0ef589b31298b3c81c135db8c39d5ddb9359cd23
c121034t@icloud.com <c121034t@icloud.com>
2020-07-07 12:32:26 +0900

    tripleの形式で抽出するように変更

--- tripleExtraction.py
+++ tripleExtraction.py
@@ -1,16 +1,17 @@
 import re
 
-def argIndexList(predict_result:dict): #引数には文章ごとのpredictor.predict（AllenNLP）の結果の連想配列を与え，{文1:{verb:(arg0,arg1)},文2:{verb:...}}を返す
-    triples = {}
-    for i,result in predict_result.items():
-        triples.setdefault(i,{})
-        for verb in result['verbs']:
-            ARG0 = [j for j,tag in enumerate(verb['tags']) if re.match('([BI]-)ARG0',tag)]
-            ARG1 = [k for k,tag in enumerate(verb['tags']) if re.match('([BI]-)ARG1',tag)]
-            if ARG0 != [] and ARG1 != []:
-                ARG0 = ' '.join([result['words'][l] for l in ARG0])
-                ARG1 = ' '.join([result['words'][m] for m in ARG1])
-                triples[i].setdefault(verb['verb'],(ARG0, ARG1))
-            else:
-                pass
+from allennlp.predictors.predictor import Predictor
+import allennlp_models.structured_prediction
+predictor = Predictor.from_path("https://storage.googleapis.com/allennlp-public-models/openie-model.2020.03.26.tar.gz")
+
+def tripleExtraction(sentence):
+    result = predictor.predict(sentence = sentence)
+    triples = []
+    for verb in result['verbs']:
+        ARG0 = [j for j,tag in enumerate(verb['tags']) if re.match('([BI]-)ARG0',tag)]
+        ARG1 = [k for k,tag in enumerate(verb['tags']) if re.match('([BI]-)ARG1',tag)]
+        if ARG0 != [] and ARG1 != []:
+            ARG0 = ' '.join([result['words'][l] for l in ARG0])
+            ARG1 = ' '.join([result['words'][m] for m in ARG1])
+            triples.append((ARG0, verb['verb'], ARG1))
     return triples
