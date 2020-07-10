71c8fa20a2458a563b3453662283140f490f76c0
c121034t@icloud.com <c121034t@icloud.com>
2020-07-10 14:27:13 +0900

    ファイルを読み込みDFに流すまでの機能をまとめた

---
+++ loadFile.py
@@ -0,0 +1,36 @@
+import os,glob
+import pandas as pd
+from email.parser import Parser
+
+def getPathList(directory_path:str) -> list:
+    if os.path.isdir(directory_path) == True:
+        directory_path = directory_path + '/**'
+        path_list = glob.glob(directory_path, recursive=True)
+        file_list = []
+        dir_list = []
+        for path in path_list:
+            if os.path.isfile(path) == True:
+                file_list.append(path)
+            else:
+                dir_list.append(path)
+        return file_list, dir_list
+    else:
+        print("{} is not a directory path.".format(directory_path))
+
+def fileToDataFrame(file_list:list) -> pd.DataFrame:
+    cols = ['Path', 'Message-ID', 'Date', 'From', 'To', 'Cc', 'Bcc', 'X-From', 'X-To', 'X-cc', 'X-bcc', 'Subject', 'Message']
+    df = pd.DataFrame(index=[], columns=cols)
+    
+    for file in file_list:
+        with open(file) as f:
+            mail = Parser().parse(f)
+        record = {}
+        for header in cols:
+            if header == 'Path':
+                record[header] = file
+            elif header == 'Message':
+                record[header] = mail.get_payload()
+            else:
+                record[header]=mail.get(header)
+        df = df.append(record, ignore_index=True)
+    return df
