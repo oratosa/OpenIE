d5efc82804cdcee9bf56ea44c6003ab6ba4d7b1d
c121034t@icloud.com <c121034t@icloud.com>
2020-07-14 21:01:30 +0900

    固有表現を含むトリプルの抽出関数を作成

---
+++ extraction.py
@@ -0,0 +1,48 @@
+import re
+
+import spacy 
+nlp = spacy.load("en_core_web_sm")
+
+from allennlp.predictors.predictor import Predictor
+import allennlp_models.structured_prediction
+predictor = Predictor.from_path("https://storage.googleapis.com/allennlp-public-models/openie-model.2020.03.26.tar.gz")
+
+def tripleExtraction(sentence):
+    #tripleの生成
+    result = predictor.predict(sentence = sentence)
+    triples = []
+    for verb in result['verbs']:
+        ARG0 = [j for j,tag in enumerate(verb['tags']) if re.match('([BI]-)ARG0',tag)]
+        ARG1 = [k for k,tag in enumerate(verb['tags']) if re.match('([BI]-)ARG1',tag)]
+        if ARG0 != [] and ARG1 != []:
+            ARG0 = ' '.join([result['words'][l] for l in ARG0])
+            ARG1 = ' '.join([result['words'][m] for m in ARG1])
+            triples.append((ARG0, verb['verb'], ARG1))
+        else:
+            pass
+    return triples
+
+def entityExtraction(sentence):
+    #Named entityを抽出する
+    doc = nlp(sentence)
+    entities = []
+    entities_type = []
+    for entity in doc.ents:
+        entities.append(entity.text)
+        entities_type.append(entity.label_)
+    return entities, entities_type
+
+def NERtripleExtraction(triples, entities):
+    NER_triples = []
+    for triple in triples:
+        for entity in entities:
+            entity = str(entity)
+            if (entity in triple[0]) or (entity in triple[2]):
+                NER_triples.append(triple)
+            else:
+                pass
+    NER_triples = list(dict.fromkeys(NER_triples))
+    if NER_triples != []:
+        return NER_triples
+    else:
+        pass
