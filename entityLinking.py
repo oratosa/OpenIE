5ed51520534b6a8dd9e8123946c3446f174f005d
c121034t@icloud.com <c121034t@icloud.com>
2020-08-04 13:28:35 +0900

    media wikiのAPIを使って,wikiのURLを取得するメソッドを実装した #19

--- entityLinking.py
+++ entityLinking.py
@@ -1,9 +1,11 @@
 #%%
 # https://sobigdata.d4science.org/group/tagme/tagme-help
+# https://www.mediawiki.org/wiki/API:Query/ja
 
 import requests
 import pprint
 
+#Parameters on tagme
 with open('gcube-token.tagme') as f:
     GCUBE_TOKEN = f.readline()
 
@@ -17,8 +19,16 @@ default_epsilon=0.3
 
 default_rho=0.1
 
-tagme_endpoint = 'https://tagme.d4science.org/tagme/tag'
+tagme_endpoint='https://tagme.d4science.org/tagme/tag'
 
+#Parameters on mediawiki
+mediawiki_endpoint='https://en.wikipedia.org/w/api.php'
+default_action='query'
+default_format='json'
+default_prop='info'
+default_inprop='url'
+
+#%%
 def tagme(text, GCUBE_TOKEN=GCUBE_TOKEN, lang=default_lang, tweet=default_tweet, include_abstract=default_include_abstract,
         include_categories=default_include_categories, include_all_spots=default_include_all_spots, long_text=default_long_text,
         epsilon=default_epsilon):
@@ -35,7 +45,7 @@ def tagme(text, GCUBE_TOKEN=GCUBE_TOKEN, lang=default_lang, tweet=default_tweet,
     response = requests.post(tagme_endpoint, params=params)
     json_res = response.json()
 
-    return json_res
+    return json_res #jsonを辞書形式にした結果
 
 #%%
 def confidentAnnotations(json_res, min_rho=default_rho):
@@ -43,4 +53,21 @@ def confidentAnnotations(json_res, min_rho=default_rho):
     for entity in json_res['annotations']:
         if entity['rho'] > min_rho:
             annotations.append(entity)
-    return annotations
+    return annotations #annotationsキーのバリューとなるentityを取り出した結果
+
+#%%
+def mediaWiki(annotations: list, action=default_action, format=default_format, prop=default_prop, inprop=default_inprop):
+    pageids = [entity['id'] for entity in annotations]
+    if len(pageids) < 50:
+        pageids = '|'.join(map(str,pageids))
+        params = {'action':default_action,
+                'format':format,
+                'prop':default_prop,
+                'inprop':default_inprop,
+                'pageids':pageids}
+        response = requests.get(mediawiki_endpoint, params=params)
+        json_res = response.json()
+        return json_res
+    else:
+        print("The size of pageids is over 50s.")
+# %%
