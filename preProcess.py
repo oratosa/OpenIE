c9248a44d363325dae02005c06a0a155b744e612
c121034t@icloud.com <c121034t@icloud.com>
2020-08-20 11:47:08 +0900

    処理内容のコメントを追加

--- preProcess.py
+++ preProcess.py
@@ -1,6 +1,8 @@
+#%%
 from nltk import tokenize
 import re
 
+#%%
 def cleaningText(message):
     cleaned_lines = []
     original_lines = message.splitlines()
@@ -17,9 +19,12 @@ def cleaningText(message):
     cleaned_message = ' '.join(map(str, cleaned_lines))
     return cleaned_message
 
+#%%
 def toSentences(message):
     cleaned_message = cleaningText(message)
     sentences = tokenize.sent_tokenize(cleaned_message)
+
+    #  改行コードがあれば空白に，空白文字が2つ以上続けば空白に，置換する
     for i,s in enumerate(sentences):
         sentences[i] = re.sub(r'\n{1,}',' ',s)
         sentences[i] = re.sub(r'\s{2,}',' ',sentences[i])
