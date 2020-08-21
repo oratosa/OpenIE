1dd4be68446be0926f20d6b464893684ced2da9c
c121034t@icloud.com <c121034t@icloud.com>
2020-08-21 14:06:30 +0900

    2020/8/20 作業分

--- preProcess.py
+++ preProcess.py
@@ -6,14 +6,24 @@ import re
 def cleaningText(message):
     cleaned_lines = []
     original_lines = message.splitlines()
+    check1 = re.compile(r'^(\W{2,}.+)') #記号が2連続以上続く
+    check2 = re.compile(r'^(_{2,}.+)') #_が2連続以上続く
+    check3 = re.compile(r'^(On \w{3}, \w{3} [0-9]{1,2}, [0-9]{4} at [0-9]{1,2}:[0-9]{1,2} (A|P)M .+)') #先行メールを示す
     for line in original_lines:
         if line == '':
             continue
-        # 先行メールの本文を示すセンテンスが出現したら，それ以降の文章は除去する
-        elif re.match(r'^(On \w{3}, \w{3} [0-9]{1,2}, [0-9]{4} at [0-9]{1,2}:[0-9]{1,2} (A|P)M .+)',line) is None:
-            cleaned_lines.append(line)
-        else:
+        # 行のトークン数が4未満なら
+        elif len(line.split()) < 4:
+            continue
+        elif check1.match(line) is True:
+            continue
+        elif check2.match(line) is True:
+            continue
+        # 先行メールの本文を示すセンテンスが出現したら，それ以降の文章は確認しない（センテンスに含めない）
+        elif check3.match(line) is True:
             break
+        else:
+            cleaned_lines.append(line)
             
     # 行ごとに分割されているので， 1つの文章に戻し， sentence_tokenizerに入力できるようにする.
     cleaned_message = ' '.join(map(str, cleaned_lines))
