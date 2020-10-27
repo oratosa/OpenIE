0f3202c1966fa5c7aaecda3775244326a3551434
c121034t@icloud.com <c121034t@icloud.com>
2020-10-27 13:21:42 +0900

    wikidataIDの取得

--- entityLinking.py
+++ entityLinking.py
@@ -26,8 +26,7 @@ tagme_endpoint='https://tagme.d4science.org/tagme/tag'
 mediawiki_endpoint='https://en.wikipedia.org/w/api.php'
 default_action='query'
 default_format='json'
-default_prop='info'
-default_inprop='url'
+default_prop='pageprops'
 
 #%%
 def tagme(text, GCUBE_TOKEN=GCUBE_TOKEN, lang=default_lang, tweet=default_tweet, include_abstract=default_include_abstract,
@@ -50,23 +49,16 @@ def tagme(text, GCUBE_TOKEN=GCUBE_TOKEN, lang=default_lang, tweet=default_tweet,
     except json.JSONDecodeError:
         return None
 
-
 #%%
-def confidentAnnotations(json_res, min_rho=default_rho):
-    annotations = []
-    for entity in json_res['annotations']:
-        if entity['rho'] > min_rho:
-            annotations.append(entity)
-    return annotations #annotationsキーのバリューとなるentityのリスト
-
-#%%
-def mediaWiki(pageid, action=default_action, format=default_format, prop=default_prop, inprop=default_inprop):
+def getWikidataID(pageid):
     params = {'action':default_action,
-            'format':format,
+            'format':default_format,
             'prop':default_prop,
-            'inprop':default_inprop,
             'pageids':pageid}
-    response = requests.get(mediawiki_endpoint, params=params)
-    json_res = response.json()
-    return json_res
+    try:
+        response = requests.get(mediawiki_endpoint, params=params)
+        json_res = response.json()
+        return json_res
+    except json.JSONDecodeError:
+        return None
 # %%
