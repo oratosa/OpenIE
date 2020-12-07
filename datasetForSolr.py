1988e7c571095a4f7d92b58b937f4b4b480cf81a
c121034t@icloud.com <c121034t@icloud.com>
2020-12-07 18:54:54 +0900

    ベースラインのsolr用に検索対象データをjson化する処理

---
+++ datasetForSolr.py
@@ -0,0 +1,21 @@
+#%%
+import pandas as pd
+import pprint
+import json
+#%%
+from db import connect
+engine = connect()
+mail_df = pd.read_sql(sql='SELECT * FROM mail_3 ORDER BY docno ASC',con=engine, index_col=None)
+#%%
+mail_df_json = mail_df.to_json(orient='index')
+mail_df_json = json.loads(mail_df_json)
+# %%
+output = []
+for document in mail_df_json.values():
+    output.append(document)
+# %%
+path = '/Users/taroaso/myprojects/OpenIE/trec/2005/json/mails3.json'
+
+with open(path, mode='w') as f:
+    f.write(str(output))
+# %%
