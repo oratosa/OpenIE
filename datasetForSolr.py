102d5f1dd47e5b0bf0dd13f557fb074c0312c73f
c121034t@icloud.com <c121034t@icloud.com>
2020-12-22 16:30:01 +0900

    solr用のjsonデータ出力のための修正

--- datasetForSolr.py
+++ datasetForSolr.py
@@ -5,7 +5,7 @@ import json
 #%%
 from db import connect
 engine = connect()
-mail_df = pd.read_sql(sql='SELECT * FROM mail_3 ORDER BY docno ASC',con=engine, index_col=None)
+mail_df = pd.read_sql(sql='SELECT * FROM mail_12_original ORDER BY docno ASC',con=engine, index_col=None)
 #%%
 mail_df_json = mail_df.to_json(orient='index')
 mail_df_json = json.loads(mail_df_json)
@@ -14,7 +14,7 @@ output = []
 for document in mail_df_json.values():
     output.append(document)
 # %%
-path = '/Users/taroaso/myprojects/OpenIE/trec/2005/json/mails3.json'
+path = '/Users/taroaso/myprojects/OpenIE/trec/2005/json/mails12.json'
 
 with open(path, mode='w') as f:
     f.write(str(output))
