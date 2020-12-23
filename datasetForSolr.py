aeb141deb775821c48f0c8904d048394f5a83e07
c121034t@icloud.com <c121034t@icloud.com>
2020-12-23 17:26:09 +0900

    solr用データ作成

--- datasetForSolr.py
+++ datasetForSolr.py
@@ -1,21 +1,21 @@
-#%%
+#
 import pandas as pd
 import pprint
 import json
-#%%
+#
 from db import connect
 engine = connect()
-mail_df = pd.read_sql(sql='SELECT * FROM mail_12_original ORDER BY docno ASC',con=engine, index_col=None)
-#%%
+mail_df = pd.read_sql(sql='SELECT * FROM mail_17_original ORDER BY docno ASC',con=engine, index_col=None)
+#
 mail_df_json = mail_df.to_json(orient='index')
 mail_df_json = json.loads(mail_df_json)
-# %%
+#
 output = []
 for document in mail_df_json.values():
     output.append(document)
-# %%
-path = '/Users/taroaso/myprojects/OpenIE/trec/2005/json/mails12.json'
+#
+path = '/Users/taroaso/myprojects/OpenIE/trec/2005/json/mails17.json'
 
 with open(path, mode='w') as f:
     f.write(str(output))
-# %%
+#
